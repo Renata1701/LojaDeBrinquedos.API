4108c3e154a3ef7ddbc9f08b02b59122e67683e4
Renata Natalli <renatanatalli.sc@gmail.com>
2025-07-09 18:34:53 -0300

    Delete LojaDeBrinquedos2/Controllers/Vendas e Clientes directory

--- LojaDeBrinquedos2/Controllers/Vendas e Clientes/Cliente.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace LojaDeBrinquedos2.Controllers.Vendas_e_Clientes;
-[Route("api/[controller]")]
-[ApiController]
-public class Cliente : ControllerBase
-{
-}
