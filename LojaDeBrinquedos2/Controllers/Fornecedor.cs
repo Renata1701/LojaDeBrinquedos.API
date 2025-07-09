6031daa2bd0c5739e29381f73f44d157d5d58ef3
Renata Natalli <renatanatalli.sc@gmail.com>
2025-07-09 18:38:46 -0300

    Delete LojaDeBrinquedos2/Controllers/Fornecedor.cs

--- LojaDeBrinquedos2/Controllers/Fornecedor.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace LojaDeBrinquedos2.Controllers;
-[Route("api/[controller]")]
-[ApiController]
-public class Fornecedor : ControllerBase
-{
-}
